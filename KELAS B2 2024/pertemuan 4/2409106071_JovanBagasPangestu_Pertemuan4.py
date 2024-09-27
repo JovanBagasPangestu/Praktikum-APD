7593ca2b188c50682538d4bde6f1c34a6f5ac0c9
JovanBagasPangestu <pjovanbagas@gmail.com>
2024-09-27 08:07:10 +0100

    pertemuan 4

---
+++ KELAS B2 2024/pertemuan 4/2409106071_JovanBagasPangestu_Pertemuan4.py
@@ -0,0 +1,65 @@
+# ulang = 10
+# for i in range(ulang)
+#     print("perulangan ke-" + str(i))
+#     print("perulangan ke--(i) kali")
+
+# for(in range (1,11,2))
+#     print("perulangan ke--(i) kali")
+
+# simpan=(12, "ufin petot", 14.5, True, 'A')
+# for i in simpan:
+#     print(i)
+
+# print(simpan(2))
+
+# for i in range(1,4):
+#     for j in range(1,4):
+#         print(f"{i}x{j}={i*j}")
+#     print()
+
+# contoh soal
+# for i in range (10, -1, -1)
+#     print("anak ayam turun (i)")
+
+# ulang = "ya"
+# hitung = 0
+# while ulang == "ya"
+#     print("perulangan ke- {hitung}")
+#     ulang = input("apakah kamu masih ingin mengulang?")
+# print("perulangan selesai")
+
+# while x < 0
+#     print(x)
+#     #  x+=1
+
+# hitung= 1
+# while true:
+#     print(f"perulangan ke {hitung}")
+#     ulang = input("masih mau mengulang?")
+#     if ulang == "tidak"
+#         print("perulangan berhenti")
+#         break
+#     hitung +=1
+
+# print(f"total perulangan: {hitung}")
+
+# for i in range (10):
+#     if i % 2 == 0:
+#         continue
+#     if i == 5:
+#         break
+#     print (i)
+
+# usn = "admin"
+# pw = "123"
+# salah = 0
+
+# while salah < 3
+#     username = input("masukan username: ")
+#     password = input("masukkan password: ")
+#     if usn == username and pw == password
+#         print("login berhasil")
+#         break
+#     else
+#         print("login gagal")
+#         salah == 1
