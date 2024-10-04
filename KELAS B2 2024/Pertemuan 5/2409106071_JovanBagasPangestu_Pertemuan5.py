0c4e2abde2313df15dfc2d4dd182d9b511772840
JovanBagasPangestu <pjovanbagas@gmail.com>
2024-10-04 15:29:14 +0800

    pertemuan 5

---
+++ KELAS B2 2024/Pertemuan 5/2409106071_JovanBagasPangestu_Pertemuan5.py
@@ -0,0 +1,89 @@
+#membuat list
+# list1 = ["a",
+#          2,
+#          True,
+#          [1,2,3]]
+
+# print(list1[-1][2])
+
+#menampilkan list
+# tas = ["buku", 32, True, 3.14, ["IF", 24]]
+
+# print(tas)
+# for i in range (len(tas)):
+#     print(tas[i])
+
+
+# matkul = ["kalkulus", "fisdas", "pti"]
+# print(matkul)
+
+# matkul.append("sastra mesin")
+# matkul[3] = "sastra mesin"
+
+# # menambahkan elemen pada posisi tertentu
+# matkul.insert(1,"sastra mesin")
+
+# matkul[1] = "fisika dasar"
+# print(matkul)
+
+# # menghapus elemen
+# del matkul [1]
+# print(matkul)
+
+# # menghapus elemen yang nilainya sama
+# buang = matkul.pop(1)
+# print(matkul)
+
+# print(f"variabel buang = {buang}")
+
+
+# prodi = ["informatika", "sistem informasi", "teknik arsitektur", "teknik lingkungan", "teknik pertambangan", "teknik elektro", "teknik industri", "teknik sipil", "teknik geologi", "teknik kimia" ]
+
+#loncat tiap 2
+# print(prodi[0:10:2])
+# print(prodi[1;2], "\n", prodi)
+
+
+# huruf = ["a", "b", "c"]
+# angka = ["1", "2", "3"]
+
+# #menggabungkan list huruf dan angka
+# gabung = huruf + angka
+# print(huruf, "\n")
+# print(angka, "\n")
+# print(gabung)
+
+# barang = [["sepatu", "jam", "kaos"],[ "pensil", "pulpen", "peraut"]]
+
+# # print(barang [1][1])
+# # print(barang[-2][-1])
+
+# for i in barang:
+#     # print(i)
+#     for j in i:
+#         print(j)
+
+#tuple
+# alat = []
+# # print(alat)
+
+# alat.append("kuku")
+# print(alat)
+
+# barang1 = {"sepatu", "jam", "kaos"}
+# barang2 = {"pensil", "pulpen", "peraut"}
+
+# totalbarang = [["sepatu", "jam", "kaos"],[ "pensil", "pulpen", "peraut"]]
+# print(totalbarang)
+
+# mhsw = (71, "informatika", "071", "jopan bagas" )
+# # print(mhsw[0:3])
+
+# absen, prodi, nim, nama = mhsw
+# print(absen)
+# print(prodi)
+# print(nim)
+# print(nama)
+
+
+
