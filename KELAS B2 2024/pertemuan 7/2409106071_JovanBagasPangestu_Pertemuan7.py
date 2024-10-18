ef1a4e761d76ba2d19a1155d8795ad858925d566
JovanBagasPangestu <pjovanbagas@gmail.com>
2024-10-18 15:08:07 +0800

    Pertemuan 7

---
+++ KELAS B2 2024/pertemuan 7/2409106071_JovanBagasPangestu_Pertemuan7.py
@@ -0,0 +1,60 @@
+#awal
+# def pungsi ():
+#     print("halo dunia")
+
+# pungsi()
+
+#contoh
+# def tambah(a,b):
+#     hasil = a+b
+
+#     print(hasil)
+
+# tambah(10,2)
+
+#return mengembalikan
+# def tambah(a,b):
+#     hasil=a+b
+#     return hasil
+# # print(tambah(7,7))
+# tambahan = tambah(5,6)+tambah(7,1)
+# print(tambahan)
+
+# def pilihan(opsi):
+#     match opsi:
+#         case 1:
+#             print(1)
+#             return
+#         case 2:
+#             print(2)
+#             return
+#     print(3)
+
+#pilihan (2)
+
+#bintang untuk menambahkan
+# def cetak (nama, nim, *matkul):
+#     print(nama, nim, matkul)
+
+# cetak("jopan", 71, "basis data", "fisdas", "jerman")
+# import os
+# os.system ("cls")
+
+# # membuat variabel global
+# Nama = "Informatika" 
+# Mata_Kuliah = "Algoritma dan Pemrograman Dasar"
+# kelas = "keren coy"
+# # membuat variabel lokal
+# def info():
+#     Nama = "Teknik Elektro"
+#     Mata_Kuliah = "Pengantar Teknik ELektro"
+#     # mengakses variabel lokal
+#     print("Prodi:", Nama)
+#     print("Mata Kuliah:", Mata_Kuliah)
+#     print(kelas)
+
+# # mengakses variabel global
+# print("Prodi:", Nama)
+# print("Mata Kuliah:", Mata_Kuliah)
+# # memanggil fungsi info
+# info()
